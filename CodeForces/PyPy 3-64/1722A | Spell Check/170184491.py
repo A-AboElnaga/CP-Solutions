d4b0ee3228b658d07939a98b4db4e70c6c127d13
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:38:39 +0200

    Committing files

---
+++ CodeForces/PyPy 3-64/1722A | Spell Check/170184491.py
@@ -0,0 +1,14 @@
+t=int(input())
+l = 5
+name ="".join(sorted('Timur'))
+for _ in range(t):
+   c_l= int(input())
+   check ="".join(sorted(input()))
+   if c_l ==l:
+     if check == name:
+       print('YES')
+     else:       print('NO')
+   else:
+       print('NO')
+ 
+   
