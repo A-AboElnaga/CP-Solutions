0e58f1c282ec28d07665644bf7cd3d81a45b3556
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:29:40 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1764C | Doremy's City Construction/182690075.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <valarray>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <deque>
+ 
+#define FIn    \
+  cin.tie(0);  \
+  cout.tie(0); \
+  ios_base::sync_with_stdio(false);
+#define endl '\n'
+using namespace std;
+ 
+/*
+int main()
+{
+  FIn;
+  int t; cin >>t;
+  while(t--){
+      int w0,h0;//overflow?
+      cin >> w0 >> h0;
+      int x1,y1,x2,y2; cin >> x1>>y1>>x2>>y2;
+      int w,h; cin >> w >>h;
+      if((x2-x1+w>w0 && y2-y1+h>h0) ||w> w0 || h >h0 )
+        cout << -1 <<endl;
+      else{
+        int xn,yn;
+        if(x1>=w-x2) xn=0; else xn=x2;
+        if(y1>=h-y2) yn=0; else yn=y2;
+        long long r1,r2,r3,rx,ry;
+        double long rslt{1e17};
+        if(xn>=x2 || yn >=y2 || xn+w<=x1 || yn+h<=y2)
+           cout << 0 <<endl;
+           continue;
+        //UR corner
+ 
+        else if(xn<x2 && yn < y2 && xn>x1 && yn>y1){
+            rx = min(x2-xn+w,x1);
+            r1 =
+            r2 =
+            r3 =
+            rslt = sqrt(min{r1,r2,r3});
+        }
+        //UL corner
+        else if(xn+w<x2 && yn < y2 && xn+w>x1 && yn>y1){
+            r1 =
+            r2 =
+            r3 =
+            rslt = sqrt(min{r1,r2,r3});
+        }
+        //BR corner
+        else if(xn<x2 && yn+h < y2 && xn>x1 && yn+h>y1){
+            r1 =
+            r2 =
+            r3 =
+            rslt = sqrt(min{r1,r2,r3});
+        }
+        //BL corner
+        else if(xn+w<x2 && yn+h < y2 && xn+w>x1 && yn+h>y1){
+            r1 =
+            r2 =
+            r3 =
+            rslt = sqrt(min{r1,r2,r3});
+        }
+      }
+  }
+  return 0;
+}
+*/
+ 
+int main()
+{
+  FIn;
+  int t; cin >>t;
+  while(t--){
+    map<int,int> m_all;
+    map<int,int> m;
+    int n; cin >>n;
+    vector<long long> v(n);
+    for(int i=0; i < n;i++){
+      cin >> v[i];
+      m_all[v[i]]+=1;
+    }
+    sort(v.begin(),v.end());
+    long long rslt=0;
+    long long mx =n/2;
+    for(long long i=0; i <n; i++){
+        m[v[i]]+=1;
+        if(m_all[v[i]]-m[v[i]]==0){
+          rslt = (n-i-1)*(i+1);
+          mx = max(rslt,mx);
+        }
+    }
+    cout << mx << endl;
+  }
+  return 0;
+}
