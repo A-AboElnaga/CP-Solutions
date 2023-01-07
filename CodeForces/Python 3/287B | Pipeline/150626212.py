803345362c265d421932531f67583c074785ce08
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:43:45 +0200

    Committing files

---
+++ CodeForces/Python 3/287B | Pipeline/150626212.py
@@ -0,0 +1,17 @@
+ 
+n, k= list(map(int,str.split(input())))
+ 
+max_pos=(((k-1)*k)//2)+1
+if n==1:
+  print(0)
+elif n<=k:
+  print(1)
+elif n>max_pos:
+  print(-1)
+# elif n==max_pos:
+#   print(k-1)
+else:
+  m=((1+(1-(4*(n-max_pos)*2))**0.5)/2)
+  if m*(m-1)+n>max_pos:
+    m-=1
+  print(int(k-m))
