046bc08af31a7b1cbe4f7e91af3e814a408552b7
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:35:22 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1742E | Scuza/176059953.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <valarray>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <deque>
+#define FIn    \
+  cin.tie(0);  \
+  cout.tie(0); \
+  ios_base::sync_with_stdio(false);
+#define endl '\n'
+using namespace std;
+ 
+long long gcd(long long a, long long b);
+long long lcm(long long a, long long b);
+unsigned long long FastPow(unsigned long long a, unsigned long long b, unsigned long long c);
+bool check(pair<int, int> in_matchings, pair<int, int> x_y);
+double func(double x, double t1, double x1, double t2, double x2);
+double zeroFinder(double min, double max, double error, double t1, double x1, double t2, double x2);
+int biSearch(long long x, vector<long long> &v, int b);
+bool isPrime(int x);
+ 
+int main()
+{
+  FIn;
+  int t; cin >>t;
+  while(t--){
+    int n,q; cin >> n >>q;
+    vector<long long> v(n+1); v[0]=0;
+    vector<pair<int/*val*/,int/*indx*/>> v2;
+    int mx{};
+    for(int i =1; i<=n;i++){
+      cin >> v[i];
+      if(v[i]>mx) v2.push_back(make_pair(v[i],i));
+      mx=max(mx,(int)v[i]);
+      v[i]+=v[i-1];
+    }
+    while(q--){
+        int k; cin >>k;
+        if (k>=mx){cout << v[n] <<' '; continue;}
+        auto  rslt = upper_bound(v2.begin(),v2.end(),make_pair(k,n));
+        cout <<v[rslt->second-1]<<' ';
+    }
+    cout << endl;
+ 
+  }
+  return 0;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+long long gcd(long long a, long long b)
+{
+  if (b == 0 || a == 0)
+    return max(a, b);
+  return gcd(b, a % b);
+}
+ 
+long long lcm(long long a, long long b)
+{
+  return (abs(a * b) / gcd(a, b));
+}
+ 
+unsigned long long FastPow(unsigned long long a, unsigned long long b, unsigned long long c = 1)
+{
+  int mod = 1;
+  while (true)
+  {
+    if (b == 0)
+      return c;
+    int r = b % 2;
+    b = b / 2;
+    (r) ? c = ((a % mod) * (c % mod)) % mod : c;
+    a = ((a % mod) * (a % mod)) % mod;
+  }
+}
+ 
+bool check(pair<int, int> in_matchings, pair<int, int> x_y)
+{
+  if (x_y.second <= in_matchings.first)
+    return false;
+  if (x_y.first >= in_matchings.second)
+    return false;
+  return true;
+}
+ 
+double func(double x, double t1, double x1, double t2, double x2)
+{
+  return t2 - t1 + fabs(x - x2) - fabs(x - x1);
+}
+ 
+double zeroFinder(double min, double max, double error, double t1, double x1, double t2, double x2)
+{
+ 
+  if ((max - min) < error)
+  {
+    return min;
+  }
+  double x = (max + min) / (double)2.0;
+  double val = func(x, t1, x1, t2, x2);
+  if (val < 0)
+  {
+    min = x;
+  }
+  else if (val > 0)
+  {
+    max = x;
+  }
+  else
+    return x;
+ 
+  return zeroFinder(min, max, error, t1, x1, t2, x2);
+}
+ 
+int biSearch(long long x, vector<long long> &v, int a)
+{
+  int b = v.size() - 1, prev = -1;
+  while (a <= b)
+  {
+    int m = (a + b) / 2;
+    if (v[m] == x)
+      return m;
+    if (v[m] < x)
+    {
+      prev = m;
+      a = m + 1;
+    }
+    else
+    {
+      b = m - 1;
+    }
+  }
+  return prev;
+}
+ 
+bool isPrime(int x)
+{
+  if ((x % 2 == 0 && x != 2) || x < 2)
+    return false;
+  for (int i = 3; i * i <= x; i += 2)
+  {
+    if (x % i == 0)
+      return false;
+  }
+  return true;
+}
