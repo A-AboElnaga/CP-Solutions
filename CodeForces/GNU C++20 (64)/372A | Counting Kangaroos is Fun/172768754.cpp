018156a6b3a874bdaba364b376db55d9b2c374c1
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:37:04 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/372A | Counting Kangaroos is Fun/172768754.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#define FIn    \
+  cin.tie(0);  \
+  cout.tie(0); \
+  ios_base::sync_with_stdio(false);
+#define endl '\n'
+using namespace std;
+ 
+long long gcd(long long a, long long b)
+{
+  if (b == 0 || a == 0)
+    return max(a, b);
+  return gcd(b, a % b);
+}
+long long lcm(long long a, long long b)
+{
+  return (abs(a * b) / gcd(a, b));
+}
+unsigned long long FastPow(unsigned long long a, unsigned long long b, unsigned long long c = 1)
+{
+  int mod = 1;
+  while (true)
+  {
+    if (b == 0)
+      return c;
+    int r = b % 2;
+    b = b / 2;
+    (r) ? c = ((a % mod) * (c % mod)) % mod : c;
+    a = ((a % mod) * (a % mod)) % mod;
+  }
+}
+ 
+int main()
+{
+  FIn;
+  int n;
+  cin >> n;
+  long rslt = n;
+  vector<long> v(n);
+  for (long i = 0; i < n; i++)
+    cin >> v[i];
+  sort(v.begin(), v.end());
+  int j=n/2;
+  for (long i = 0; i < (n / 2); i++)
+  {
+    for(;;){
+      if (v[j] >= v[i] * 2)
+      {
+        rslt -= 1;
+        j += 1;
+        break;
+      }
+      else{j+=1;}
+      if(j==n) break;
+    }
+    if(j==n) break;
+    
+  }
+  cout << rslt;
+  return 0;
+}
