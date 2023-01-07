361a5907c39efc53ab8f38055aec80c5a3f19fd1
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:39:38 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/431A | Black Square/169621206.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stdio.h>
+#include <ctype.h>
+ 
+ 
+using namespace std; 
+ 
+#define FIn     cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0)
+ 
+int main(){
+    FIn;
+    int arr[5]{},rslt{},val{};
+    string s;
+    for (size_t i = 1; i < 5; i++)
+        {cin >> arr[i];}
+    cin >> s;
+    for(auto j =0; j < s.size();j++ ){
+        rslt += arr[s[j]- '0'];
+    }
+    cout <<rslt;
+    return 0;
+}
