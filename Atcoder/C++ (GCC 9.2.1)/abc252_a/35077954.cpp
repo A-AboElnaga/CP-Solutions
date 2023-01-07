19e37ed42e5ca211f2f19a5740f9fe5f649e0c4b
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:22:15 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc252_a/35077954.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#define FIn  cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
+#define endl '\n'
+using namespace std;
+
+
+long long gcd(long long a, long long b){
+  if (b==0 || a==0 ) return max(a,b);
+  return gcd(b,a%b);
+} 
+long long lcm(long long a, long long b){
+  return (abs(a*b)/gcd(a,b));
+}
+unsigned long long FastPow(unsigned long long a, unsigned long long b, unsigned long long c=1){
+  int mod=1;
+  while(true){
+  if (b == 0) return c;
+  int r = b%2 ; b=b/2;
+  (r)? c=((a%mod)*(c%mod))%mod: c;
+  a=((a%mod)*(a%mod))%mod;
+  }
+}
+
+
+int main()
+{  
+  FIn;
+  int n; cin >> n;
+  char c= n;
+  cout << c;
+ return 0;
+}
