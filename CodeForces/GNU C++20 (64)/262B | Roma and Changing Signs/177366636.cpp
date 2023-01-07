25a2a605c9c31d2cd6cac7f1c0e90549eed9eaeb
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:31:38 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/262B | Roma and Changing Signs/177366636.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <valarray>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <deque>
+ 
+#define FIn    cin.tie(0);  cout.tie(0); ios_base::sync_with_stdio(false);
+#define endl '\n'
+using namespace std;
+ 
+ 
+int main()
+{
+  FIn;
+  int n,k; cin >> n >> k;
+  vector<int> neg;
+  long sm_neg{};
+  long sm_pos{};
+  int mn =1000000;
+  for(int i =0 ; i < n; i++){
+    int x; cin >> x;
+    mn = min(mn,abs(x));
+    if(x>=0) {sm_pos+=x;}
+    else     {neg.push_back(-x); sm_neg-=x;}
+  }
+  if(k==neg.size())cout << sm_neg+sm_pos;
+  else if(k<neg.size()) cout << sm_pos-sm_neg+ 2* accumulate(neg.begin(),next(neg.begin(),k),0);
+  else {int rslt = sm_neg+sm_pos-((k-neg.size())%2)*mn*2; cout << rslt;}
+ 
+  return 0;
+}
