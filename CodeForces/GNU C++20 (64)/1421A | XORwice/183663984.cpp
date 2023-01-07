eab7ad2450c4b523e37a85201d3313c98d93af88
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-01-07 12:28:36 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1421A | XORwice/183663984.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <valarray>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <deque>
+ 
+#define FIn    \
+  cin.tie(0);  \
+  cout.tie(0); \
+  ios_base::sync_with_stdio(false);
+#define endl '\n'
+using namespace std;
+ 
+int main()
+{
+  FIn;
+  int t; cin >> t;
+  while(t--){
+    int a, b; cin >> a >>b; 
+    a = a^b;
+    cout << a <<endl;
+ 
+  }
+  return 0;
+}
