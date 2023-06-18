dbec38c4256b6a9cbbb685423672e1ccc0bda0ce
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 23:59:39 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1840A | Cipher Shifer/208986831.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <bitset>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+ 
+#define file                                                                   \
+  freopen("output.txt", "w", stdout);                                             \
+  freopen("input.txt", "r", stdin)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+int tc;
+int n;
+string s;
+void pre()
+{
+ 
+}
+void solve(){
+    cin >> n;
+    cin >> s;
+    char x=s[0];
+    for(int i=1;i<n;i++){
+        if(s[i]==x){
+            cout << x;
+            if(i+1<n) x=s[i+1];
+            i++;
+        }
+    }
+    cout << endl;
+}
+ 
+int main() {
+    FIn;
+    //file;
+    tc = 1;
+    cin >> tc;
+    pre();
+    while (tc--) {
+        solve();
+    }
+}
