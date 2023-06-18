668a0e9d3166745993c29f71046932c0fa36881e
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:31:33 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1807A | Plus or Minus/198116336.cpp
@@ -0,0 +1,57 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+c;
+
+(){
+gcd(a,b)==1){
+<< a << ' ' << b << endl;;
+ b+=1;
+
+ gcd(a,b);
+
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t=1;
+  cin >> t;
+  while(t--){
+    cin >> a >> b >> c;
+    if(a+b==c){
+      cout << '+' << endl;
+    }
+    else{
+      cout << '-'<< endl;
+    }
+    // solve();
+  }
+}
