9c8c1a9cbf3fe227037b6d3a4b342a223985761d
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 22:51:22 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1834B | Maximum Strength/210041750.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+#define ll long long
+#define file                                                                   \
+  freopen("input.txt", "r", stdin);\
+  freopen("output.txt", "w", stdout)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+ 
+const int mod=1e9+7;
+int tc;
+int n;
+string l,r;
+void pre();
+void solve() {
+    cin >> l >> r;
+    while(l.size()<r.size()) l="0"+l;
+    n=l.size();
+    int i;
+    int rslt=0;
+    for(i=0;i<n;i++){
+        if(l[i]-'0'<r[i]-'0'){
+            rslt+=(r[i]-'0')-(l[i]-'0');
+            break;
+        }
+    }
+    rslt+=(9*max(0,(n-i-1)));
+    cout << rslt << endl;
+}
+int main() {
+    FIn;
+//    file;
+//    freopen("key.in", "r", stdin);
+//    freopen("key.out", "w", stdout);
+    tc = 1;
+    cin >> tc;
+    pre();
+    for(int i=1;i<=tc;i++){
+        //cout << "Case " << i <<": ";
+        solve();
+    }
+}
+ 
+void pre(){
+}
+long long inv(long long i) {
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
