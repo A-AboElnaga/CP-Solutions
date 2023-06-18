698dd3696044ddb5651c68d77b1d3aececcab607
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:29:43 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1809B | Points on Plane/200962748.cpp
@@ -0,0 +1,50 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+ long n;
+ sq;
+(){
+;
+n);
+<n){sq+=1;}
+ sq-1 << endl;
+
+
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
