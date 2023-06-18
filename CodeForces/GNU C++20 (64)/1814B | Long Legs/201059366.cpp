a70415e9862a64f591475f31698318709e73f6d3
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:29:26 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1814B | Long Legs/201059366.cpp
@@ -0,0 +1,55 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+
+(){
+swap(a,b);
+=b+a;
+,f2=sqrt(b)+sqrt(a);
+ i=1;i<=f2;i++){
+min(rslt,(i-1)+b/i+(b%i!=0)+(a/i)+(a%i!=0));
+  }
+  cout << rslt << endl;
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while(t--){
+    k=1;
+    cin >> a >> b;
+    solve();
+  }
+}
