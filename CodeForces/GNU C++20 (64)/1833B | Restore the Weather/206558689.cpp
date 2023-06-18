d17b701c6d8826bbee21fb812b85c9a0a04b0ac7
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 23:59:44 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1833B | Restore the Weather/206558689.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <bitset>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+ 
+#define file                                                                   \
+  freopen("output.txt", "w", stdout);                                             \
+  freopen("input.txt", "r", stdin)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+int tc;
+int n,k,c;
+pair<int,int> a[100005],b[100005];
+int rslt[100005];
+void pre(){
+ 
+}
+void solve() {
+    cin >> n >> k;
+    for(int i=0;i<n;i++){
+        cin >> c;
+        a[i]={c,i};
+    }
+    for(int i=0;i<n;i++){
+        cin >> c;
+        b[i]={c,i};
+    }
+    sort(a,a+n);
+    sort(b,b+n);
+    for(int i=0;i<n;i++){
+        rslt[a[i].second]=b[i].first;
+    }
+    for(int i=0;i<n;i++){
+        cout << rslt[i] << ' ';
+    }cout << endl;
+ 
+}
+ 
+int main() {
+    FIn;
+    //file;
+    tc = 1;
+    cin >> tc;
+    pre();
+    while (tc--) {
+        solve();
+    }
+}
