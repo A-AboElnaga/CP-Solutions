9267ef2dab61002d86874acacb5705ce32031279
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 23:59:15 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1831A | Twin Permutations/209087885.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <bitset>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+ 
+#define file                                                                   \
+  freopen("output.txt", "w", stdout);                                             \
+  freopen("input.txt", "r", stdin)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+int tc;
+int n,x;
+void pre()
+{
+ 
+}
+ 
+void solve(){
+    cin >> n;
+    for(int i =0;i<n;i++){
+       cin >> x;
+       cout << n-x+1 << ' ';
+    }cout << endl;
+}
+ 
+int main() {
+    FIn;
+    //file;
+    tc = 1;
+    cin >> tc;
+    pre();
+    while (tc--) {
+        solve();
+    }
+}
