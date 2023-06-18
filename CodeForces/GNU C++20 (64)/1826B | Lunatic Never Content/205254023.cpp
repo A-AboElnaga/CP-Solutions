fe23ea308b985a567cf8313f90a2c4690fea41d0
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:26:58 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1826B | Lunatic Never Content/205254023.cpp
@@ -0,0 +1,56 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+ file                                                                   \
+"output.txt", "w", stdout);                                             \
+"input.txt", "r", stdin)
+
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+
+ endl '\n'
+
+
+;
+200005];
+(){
+ n;
+int i=1;i<=n;i++){
+        cin >> arr[i];
+
+    rslt=arr[n]-arr[1];
+    for(int i=1;i<n-i+1;i++){
+    rslt=gcd(arr[i]-arr[n-i+1],rslt);
+    }
+    cout << rslt << endl;
+}
+ 
+ 
+int main() {
+    FIn;
+    //file;
+     t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
