778c36d58e474f48aca23b6248f9feb5e0e9a5b4
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 22:43:17 +0300

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_a/41485437.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <bitset>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+
+
+#define file                                                                   \
+  freopen("output.txt", "w", stdout);                                             \
+  freopen("input.txt", "r", stdin)
+
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+
+#define endl '\n'
+int t;
+int dp[(int)1e5+5];
+int arr[(int)1e5+5];
+int n;
+void pre(){
+
+}
+
+void solve(){
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cin >> arr[i];
+        dp[i]=INT_MAX;
+    }
+
+    dp[0]=0;
+    dp[1]=0;
+    for(int i=1;i<=n;i++){
+        dp[i+1]=min(dp[i+1],dp[i]+abs(arr[i]-arr[i+1]));
+        dp[i+2]=min(dp[i+2],dp[i]+abs(arr[i]-arr[i+2]));
+    }
+
+    cout << dp[n] << endl;
+}
+
+
+
+int main() {
+    FIn;
+    //file;
+    t = 1;
+    //cin >> t;
+    pre();
+    while (t--) {
+        solve();
+    }
+}
