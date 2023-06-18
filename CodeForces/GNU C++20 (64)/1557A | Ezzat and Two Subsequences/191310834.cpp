38c981284127727f4588db87804f76a56ecc38b9
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:36:05 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1557A | Ezzat and Two Subsequences/191310834.cpp
@@ -0,0 +1,42 @@
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+double n;
+
+ sa;
+ rslt;
+
+
+
+
+
+;
+--){
+1000000010;
+
+0;
+ n;
+int i=0;i<n;i++){
+>> tt;
+max(mx,tt);
+tt;
+
+ (sa-mx)/(n-1);
++= mx;
+<< setprecision(9) << fixed;
+<< rslt << endl;
+
+;
+}
+ 
+ 
