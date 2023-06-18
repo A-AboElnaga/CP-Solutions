2acd5b5e0f68e6a2630beb5cbbd75bd836068776
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:38:06 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1674B | Dictionary/188582661.cpp
@@ -0,0 +1,44 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+
+
+
+ cin >> t;
+--){
+,y; cin >> x >> y;
+ rslt =25*(x-'a')+ (y-'a');
+    if(y<x) rslt+=1;
+    cout << rslt << endl;
+  }
+return 0;
+}
