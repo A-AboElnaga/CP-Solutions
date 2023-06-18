5a7f7eb85e8028a83e30b6f06c271cdd996047dd
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:29:34 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1814A | Coins/200988173.cpp
@@ -0,0 +1,55 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+ long n,k;
+
+(){
+ n >> k;
+0){
+<< "Yes" << endl;
+
+
+==1){
+      cout << "Yes" << endl;
+    }
+    else cout << "No" << endl;
+  }
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
