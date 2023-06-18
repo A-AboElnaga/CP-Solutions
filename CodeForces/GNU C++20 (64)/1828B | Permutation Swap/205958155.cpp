e5f4447e16ac1414921b9afe6baa90dd4db156b9
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:26:14 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1828B | Permutation Swap/205958155.cpp
@@ -0,0 +1,70 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+<ext/pb_ds/assoc_container.hpp> // Common file
+<ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+namespace __gnu_pbds;
+class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+
+
+ file                                                                   \
+"output.txt", "w", stdout);                                             \
+"input.txt", "r", stdin)
+
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+
+ endl '\n'
+
+int n;
+int rslt,x;
+ 
+void pre(){
+ 
+}
+ 
+ 
+ 
+void solve(){
+    cin >> n;
+    cin >> x;
+    rslt= (x-1);
+    for(int i=2;i<=n;i++){
+        cin >> x;
+        rslt=gcd(rslt,abs(x-i));
+    }
+    cout << rslt << endl;
+}
+ 
+ 
+int main() {
+    FIn;
+    //file;
+    t = 1;
+    cin >> t;
+    pre();
+    while (t--) {
+        solve();
+    }
+}
