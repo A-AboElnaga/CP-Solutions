ce477f990eaa8501237ffa2b5b58cbdea6e6d943
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:35:39 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1611B | Team Composition: Programmers and Mathematicians/191725046.cpp
@@ -0,0 +1,46 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+long long a,b;
+ rslt;
+
+
+
+
+;
+--){
+    cin >> a >> b;
+    rslt = (a+b)/4;
+    rslt = min({rslt,a,b});
+    cout << rslt << endl;
+  }
+  return 0;
+}
