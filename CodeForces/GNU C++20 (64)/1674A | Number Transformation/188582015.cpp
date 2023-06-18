668e46c12268545b01da86f3d52787b74b4eaac7
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:38:08 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1674A | Number Transformation/188582015.cpp
@@ -0,0 +1,45 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+
+
+
+ cin >> t;
+--){
+y; cin >> x >> y;
+==0){
+      cout <<  1 <<' '<< y/x << endl;  
+    }
+    else cout << 0 <<' ' <<   0 << endl;
+  }
+return 0;
+}
