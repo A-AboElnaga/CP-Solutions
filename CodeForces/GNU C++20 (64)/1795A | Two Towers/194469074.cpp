f752a804df09785ecfb56bbdbb7a56c137c3b8d1
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:34:26 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1795A | Two Towers/194469074.cpp
@@ -0,0 +1,48 @@
+bits/stdc++.h>
+
+namespace std;
+
+ file                                                                   \
+"out.txt", "w", stdout);                                             \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+,c,d;
+ {
+
+
+;
+--){
+ n >> m;
+ a >> b;
+1;
+    reverse(b.begin(),b.end());
++b;
+    reverse(b.begin(),b.end());
+    reverse(a.begin(),a.end());
+a;
+ counter =0;
+m;
+int i=1;i<n;i++){
+[i]==c[i-1])counter+=1;
+
+counter<=1) flag=0;
+    counter =0;
+int i=1;i<n;i++){
+[i]==d[i-1])counter+=1;
+    }
+    if(counter<=1) flag=0;
+ 
+    if(flag) cout << "NO" << endl;
+    else cout << "YES" << endl;
+  }
+ 
+  return 0;
+}
