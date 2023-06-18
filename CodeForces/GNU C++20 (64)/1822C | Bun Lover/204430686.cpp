9de47f4db5395a2805c195eeb58756998358620a
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:27:25 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1822C | Bun Lover/204430686.cpp
@@ -0,0 +1,52 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>  
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+ file                                                                   \
+"output.txt", "w", stdout);                                             \
+"input.txt", "r", stdin)
+
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+
+ endl '\n'
+
+ long n;
+ ans;
+(){
+;
+){
+<< 26 << endl;
+
+
+    ans=26ll+(n*(n+1ll))-20ll+n-4ll;
+    cout << ans << endl;
+  }
+}
+int main() {
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
