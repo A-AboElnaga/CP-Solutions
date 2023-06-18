f017a59d5004b0ae5e21686c5f12c0373c10dc94
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:30:41 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1708B | Difference of GCDs/199148393.cpp
@@ -0,0 +1,64 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+r;
+100005];
+
+()
+
+ i=1;i<=n;i++){
+)*i;
+<=r && x>=l ){
+i]=x;
+    }
+    else{
+      cout << "No" << endl;
+      return;
+    }
+  }
+  cout << "Yes" << endl;
+  for(int i=1;i<=n;i++){
+    cout << arr[i] << ' ';
+  }cout << endl;
+ 
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while (t--)
+  { 
+    cin >> n >> l >> r;
+    solve();
+  }
+}
