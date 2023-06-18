c0feef650356629382af358280e9146a20a0324a
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:29:56 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1811B | Conveyor Belts/200716538.cpp
@@ -0,0 +1,55 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+,y11,x2,y2;
+
+ get_cycle(int x,int y){
+ min({x,y,n-x,n-y});
+
+()
+
+ n >> x1 >> y11 >> x2 >> y2;
+
+  x1=get_cycle(x1,y11);
+  x2=get_cycle(x2,y2);
+  cout << abs(x1-x2) << endl;
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while (t--)
+  { 
+    solve();
+  }
+}
