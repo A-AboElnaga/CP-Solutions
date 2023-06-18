048bb56937139d06ec19789deb86560021af3f4f
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 23:57:46 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1704A | Two 0-1 Sequences/209320564.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <bitset>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+ 
+#define file                                                                   \
+  freopen("input.txt", "r", stdin);\
+  freopen("output.txt", "w", stdout)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+int tc;
+int n,m;
+string s1,s2;
+void pre();
+void solve() {
+    cin >> n >> m;
+    cin >> s1 >> s2;
+    while(s2.size()>1){
+        if(*s1.rbegin()==*s2.rbegin()){
+            s1.pop_back();
+            s2.pop_back();
+        }
+        else{
+            cout <<"NO" << endl;
+            return;
+        }
+    }
+    for(auto el:s1){
+        if(el==s2[0]){
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+ 
+int main() {
+    FIn;
+    //file;
+    //freopen("input.in", "r", stdin);
+    tc = 1;
+    cin >> tc;
+    pre();
+    for(int i=1;i<=tc;i++){
+        //cout << "Case " << i <<": ";
+        solve();
+    }
+}
+ 
+void pre(){
+}
