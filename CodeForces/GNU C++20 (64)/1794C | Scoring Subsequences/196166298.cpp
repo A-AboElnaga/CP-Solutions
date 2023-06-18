514e56f1dde9b74588b7179824810a447505ef0a
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:32:40 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1794C | Scoring Subsequences/196166298.cpp
@@ -0,0 +1,61 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<sstream>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+ file                                                                   \
+"out.txt", "w", stdout);                                             \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+ endl '\n'
+
+100005];
+
+
+(){
+ i=0;i<n;i++){
+
+arr[l]<r-l){
+
+    }
+    cout << r-l << ' ';
+  }
+  cout << endl;
+}
+ 
+int main() {
+  FIn;
+  // file;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    for(int i=0;i<n;i++){
+      cin >> arr[i];
+    }
+    l=0,r=0;
+    solve();
+  }
+ 
+ 
+ return 0;
+}
