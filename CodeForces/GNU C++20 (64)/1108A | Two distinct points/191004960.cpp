d032b3f08ac03cc2f4e804b1b8eae72aaa2028e4
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:36:42 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1108A | Two distinct points/191004960.cpp
@@ -0,0 +1,48 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+<bitset>
+
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+
+// file; 
+ cin >> q;
+--){
+r,ll,rr;
+ l >> r >> ll >> rr;
+    int b=ll;
+    if(l ==ll){
+      b=rr;
+    }
+    cout << l << ' ' << b << endl; 
+  }  
+ 
+  return 0;
+}
