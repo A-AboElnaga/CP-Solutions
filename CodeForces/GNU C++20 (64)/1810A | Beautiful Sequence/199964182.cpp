7896c891dc55a0cdd9160527ecd27e4ca7fa0aed
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:30:24 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1810A | Beautiful Sequence/199964182.cpp
@@ -0,0 +1,60 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+;
+100];
+
+()
+
+;
+ i=1;i<=n;i++){
+arr[i];
+  }
+  for(int i=1;i<=n;i++){
+    if(arr[i]<=i){
+      cout << "Yes" << endl;
+      return;
+    }
+  }
+  cout << "No" << endl;
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while (t--)
+  { 
+    solve();
+  }
+}
