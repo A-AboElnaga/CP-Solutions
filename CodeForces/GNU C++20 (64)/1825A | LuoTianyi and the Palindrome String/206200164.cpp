266f7977c36a244e985a523d4e67a78da28aa1ec
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:25:46 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1825A | LuoTianyi and the Palindrome String/206200164.cpp
@@ -0,0 +1,71 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+<ext/pb_ds/assoc_container.hpp> // Common file
+<ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+namespace __gnu_pbds;
+class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+
+
+ file                                                                   \
+"output.txt", "w", stdout);                                             \
+"input.txt", "r", stdin)
+
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+
+ endl '\n'
+
+string s,tmp;
+void pre(){
+ 
+}
+void solve(){
+    cin >> s;
+    for(int i=s.size()-1;i>=2;i--){
+        for(int j=0;j+i<s.size();j++){
+            tmp=s.substr(j,i);
+            reverse(tmp.begin(),tmp.end());
+            if(tmp!=s.substr(j,i)){
+                cout << i << endl;
+                return;
+            }
+        }
+    }
+    cout << -1 << endl;
+ 
+}
+ 
+ 
+ 
+int main() {
+    FIn;
+    //file;
+    t = 1;
+    cin >> t;
+    pre();
+    while (t--) {
+        solve();
+    }
+}
