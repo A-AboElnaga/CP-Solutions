3d8ade2f71253ad50890c40c40993c310395754e
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:36:01 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1675A | Food for Animals/191529668.cpp
@@ -0,0 +1,52 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+,c,x,y;
+
+
+
+
+;
+--){
+ a >> b >> c >> x >> y;
+    x-=a; x=max(x,0);
+    y-=b; y=max(y,0);
+    if(c>= x+y){
+      cout << "Yes";
+    }
+    else cout << "No";
+    cout <<endl;
+  }
+ 
+  return 0;
+}
+ 
+ 
