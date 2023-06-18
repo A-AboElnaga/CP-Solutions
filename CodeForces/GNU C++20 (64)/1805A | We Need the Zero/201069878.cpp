0feec7b3778c0b6f34b2e79438c35f5f64afbe13
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:29:22 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1805A | We Need the Zero/201069878.cpp
@@ -0,0 +1,53 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+;
+(){
+;
+
+ i=0;i<n;i++){
+ x;
+x;
+
+0 && rslt==0){cout << 0 << endl;}
+  else if(n%2==1)cout << rslt << endl;
+  else cout << -1 << endl; 
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t = 1;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
