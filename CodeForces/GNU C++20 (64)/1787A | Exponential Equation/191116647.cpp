c3cc6e3e7e9259dc56f376cea42e40738863f1c2
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:36:33 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1787A | Exponential Equation/191116647.cpp
@@ -0,0 +1,46 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+ n;
+
+
+
+// file; 
+;
+--){
+ n;
+    if(n%2==0){
+      cout << 1 << ' ' << n/2 << endl;
+    }
+    else cout << -1 << endl;
+  }
+  return 0;
+}
