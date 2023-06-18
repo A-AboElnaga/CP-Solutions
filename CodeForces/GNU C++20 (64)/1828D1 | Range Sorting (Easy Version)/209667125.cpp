6059fe8a2818297d76e697ca86d5ac42167c8807
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 23:56:39 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1828D1 | Range Sorting (Easy Version)/209667125.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+#define ll long long
+#define file                                                                   \
+  freopen("input.txt", "r", stdin);\
+  freopen("output.txt", "w", stdout)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+ 
+const int mod=1e9+7;
+int tc;
+int n;
+int arr[200005];
+long long rslt;
+void pre();
+void solve() {
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    rslt=0;
+    for(int i=0;i<n;i++){
+        stack<int> stk;
+        for(int j=i,x;j<n;j++){
+            x=arr[j];
+            while(!stk.empty() && stk.top()>arr[j]){
+                x=max(x,stk.top());
+                stk.pop();
+            }
+            stk.push(x);
+            rslt+=(j-i)-(stk.size()-1);
+        }
+ 
+    }
+    cout << rslt << endl;
+}
+int main() {
+    FIn;
+    //file;
+    //freopen("input.in", "r", stdin);
+    tc = 1;
+    cin >> tc;
+    pre();
+    for(int i=1;i<=tc;i++){
+        //cout << "Case " << i <<": ";
+        solve();
+    }
+}
+ 
+void pre(){
+}
+long long inv(long long i) {
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
