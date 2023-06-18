c196fa03a7779d4245369364b38c6abbcda7ae27
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:31:19 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1807G2 | Subsequence Addition (Hard Version)/198239930.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <sstream>
+#include <bitset>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define file                       \
+  freopen("out.txt", "w", stdout); \
+  freopen("in.txt", "r", stdin)
+#define inf unsigned long long32_MAX
+#define FIn    \
+  cin.tie(0);  \
+  cout.tie(0); \
+  ios_base::sync_with_stdio(false)
+// #define endl '\n'
+int t,n;
+int arr[200005];
+ 
+void solve(){
+    long long mx=1;
+ 
+    if(arr[0]!=1){
+      cout << "No" << endl;
+      return;
+    }
+    for(int i=1;i<n;i++){
+      if(arr[i]<=mx){
+        mx+=(long long)arr[i];
+      }
+      else{
+        cout << "No" << endl;
+        return;
+      }
+    }
+    cout << "Yes" << endl;
+}
+ 
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t=1;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    for(int i=0;i<n;i++){
+      cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    solve();
+  }
+}
