64cc566d635ec9b11ffe49e4695625281db75a9d
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-18 22:51:54 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/603A | Alternative Thinking/209808306.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+ 
+#define ll long long
+#define file                                                                   \
+  freopen("input.txt", "r", stdin);\
+  freopen("output.txt", "w", stdout)
+ 
+#define FIn                                                                    \
+  cin.tie(0);                                                                  \
+  cout.tie(0);                                                                 \
+  ios_base::sync_with_stdio(false)
+ 
+#define endl '\n'
+ 
+const int mod=1e9+7;
+int tc;
+int n;
+string s;
+void pre();
+void solve() {
+    cin >> n;
+    cin >> s;
+    int r1,r2;
+    r1=r2=0;
+    for(int i=0,stat1=1,stat2=0;i<n;i++){
+        if(s[i]-'0'==stat1){
+            r1++;
+            stat1=1-stat1;
+        }
+        if(s[i]-'0'==stat2){
+            r2++;
+            stat2=1-stat2;
+        }
+    }
+    cout << min(n,2+max(r1,r2));
+}
+int main() {
+    FIn;
+//    file;
+//    freopen("key.in", "r", stdin);
+//    freopen("key.out", "w", stdout);
+    tc = 1;
+    //cin >> tc;
+    pre();
+    for(int i=1;i<=tc;i++){
+        //cout << "Case " << i <<": ";
+        solve();
+    }
+}
+ 
+void pre(){
+}
+long long inv(long long i) {
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
