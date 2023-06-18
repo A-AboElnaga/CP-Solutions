8b9eb2fe178c427cbc60a98035e0fcd4ef8fcd00
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:32:46 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1794A | Prefix and Suffix Array/196151161.cpp
@@ -0,0 +1,65 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<sstream>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+ file                                                                   \
+"out.txt", "w", stdout);                                             \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+string word1,word2;
+(){
+ i=0;i<n-1;i++){
+word1[i]!=word2[(n-1)-i-1]){
+      cout << "No" <<endl; return;
+
+
+  cout << "Yes" << endl;
+  return;
+ 
+}
+ 
+int main() {
+  FIn;
+  // file;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    a = 2*(n-1);
+    word1="";
+    while(a--){
+      cin >> x;
+      if(x.size()==n-1)
+       if(word1.empty()){
+        word1 = x;
+       }else word2=x;
+    }
+    solve();
+  }
+ 
+ 
+ return 0;
+}
