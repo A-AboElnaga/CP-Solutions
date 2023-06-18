e0c605b557ac26f83c17a2b4d25126ee87ac738c
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:29:47 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1812B | Was it Rated?/200914694.cpp
@@ -0,0 +1,44 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+
+
+
+// t = 1;
+// pre();
+;
+ 15 and t != 20 and t!= 21) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
