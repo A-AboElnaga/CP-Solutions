1331b5874925df67d578ac7fa14cb30a98739a1e
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:31:29 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1807C | Find and Replace/198164443.cpp
@@ -0,0 +1,61 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+<bitset>
+bits/stdc++.h>
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+];
+(){
+int i=0;i<n;i++){
+arr[s[i]-'a']==-1){
+        arr[s[i]-'a']=(i%2);
+
+ if(arr[s[i]-'a']!=i%2){
+        cout << "No" << endl;
+        return;
+      }
+    }
+    cout << "Yes" << endl;
+}
+ 
+int main()
+{
+  FIn;
+  // file;
+  // t=1;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    cin >> s;
+    for(int i=0;i<26;i++){
+        arr[i]=-1;
+    }
+    solve();
+  }
+}
