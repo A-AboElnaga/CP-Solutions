70a8fc52338d606b9293b73c53d06f74074df0b7
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:34:24 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1795B | Ideal Point/194470088.cpp
@@ -0,0 +1,40 @@
+bits/stdc++.h>
+
+namespace std;
+
+ file                                                                   \
+"out.txt", "w", stdout);                                             \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+];
+
+ {
+
+
+;
+--){
+ n >> k;
+]=0;arr[1]=0;arr[2]=0;
+
+int i=0;i<n;i++){
+>> l >> r;
+<=k && k<=r){
+        arr[1]+=1;
+(l<=k-1) arr[0]+=1;
+(k+1<=r) arr[2]+=1;
+
+
+[1]>arr[0] && arr[1]>arr[2]){
+      cout << "Yes" << endl;
+
+ cout << "No" << endl;
+
+;
+}
