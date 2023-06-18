ea0a0bd1416e59cd7205156dfe7fac7f1198ab7e
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:25:50 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1829D | Gold Rush/206139710.cpp
@@ -0,0 +1,67 @@
+<algorithm>
+bits/stdc++.h>
+<bitset>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+namespace std;
+
+<ext/pb_ds/assoc_container.hpp> // Common file
+<ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+namespace __gnu_pbds;
+class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+// find_by_order((int)k) returns iterator of the kth element
+// order_of_key((T)key) returns the number of elements less than this key
+
+
+ file                                                                   \
+"output.txt", "w", stdout);                                             \
+"input.txt", "r", stdin)
+
+FIn                                                                    \
+0);                                                                  \
+(0);                                                                 \
+::sync_with_stdio(false)
+
+ endl '\n'
+
+int n,m;
+priority_queue<int> pq;
+void pre(){
+ 
+}
+bool solve(int x){
+ 
+        if(x==m){return true;}
+        if(x%3!=0){return false;}
+        else{
+            return (solve(x/3) || solve(x/3 *2));
+        }
+}
+ 
+ 
+ 
+int main() {
+    FIn;
+    //file;
+    t = 1;
+    cin >> t;
+    pre();
+    while (t--) {
+        cin >> n >> m;
+        if(solve(n)) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
