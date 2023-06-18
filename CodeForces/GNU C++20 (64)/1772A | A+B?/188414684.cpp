167da98172776d1c6093ad85cc794dfdcd50a28f
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2023-06-19 01:38:26 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1772A | A+B?/188414684.cpp
@@ -0,0 +1,43 @@
+<algorithm>
+<cmath>
+<deque>
+<functional>
+<iomanip>
+<iostream>
+<map>
+<numeric>
+<queue>
+<set>
+<stack>
+<string>
+<unordered_set>
+<utility>
+<valarray>
+<vector>
+<sstream>
+
+namespace std;
+
+ file                       \
+"out.txt", "w", stdout); \
+"in.txt", "r", stdin)
+ inf unsigned long long32_MAX
+FIn    \
+0);  \
+(0); \
+::sync_with_stdio(false)
+ endl '\n'
+
+
+
+
+
+
+ cin >> t;
+--){
+, p,b; cin >> a >> p >> b;
+<< (a-'0')+(b-'0') << endl;
+ 
+  }
+return 0;
+}
